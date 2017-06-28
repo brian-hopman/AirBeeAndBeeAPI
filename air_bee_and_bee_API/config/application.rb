02426d25207ea3430a1098b0797b0cb5fb04096f
brian-hopman <hopman_2@hotmail.com>
2017-06-28 17:37:51 -0400

    first, very permissive iteration of the backend

--- air_bee_and_bee_API/config/application.rb
+++ air_bee_and_bee_API/config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
+# require "rack-cors"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,6 +21,14 @@ module AirBeeAndBeeApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :index]
+      end
+    end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
